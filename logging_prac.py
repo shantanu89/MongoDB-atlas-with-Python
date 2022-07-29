888e136402889253ca472bb0fc52438e4b3a1db2
Shantanu89 <shantanupradhan89@gmail.com>
2022-07-29 09:43:35 +0530

    Prac commands added

---
+++ logging_prac.py
@@ -0,0 +1,13 @@
+import logging
+
+logging.basicConfig(filename="testlog.log", level=logging.INFO)
+
+logging.info("This is test logging code")
+logging.warning("This is test warning")
+
+lst = [1, 2, 3, 4, 5]
+
+for i in lst:
+    if i == 2:
+        logging.info(i)
+        logging.warning("2 found in list")
